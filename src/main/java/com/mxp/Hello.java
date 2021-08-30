3546a611c91557fe5b1038b1dae2a1df5f02b6f8
mengxiaobin <mengxiaobin@calfcredit.com>
2021-08-30 14:54:25 +0800

    提交了不少

--- src/main/java/com/mxp/Hello.java
+++ src/main/java/com/mxp/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("222222");
+        System.out.println("2222");
     }
 }
