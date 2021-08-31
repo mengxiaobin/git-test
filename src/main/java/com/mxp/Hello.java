eb97ba402a3567c09dd788f83ba81887aa83d971
mengxiaobin <89378055+mengxiaobin@users.noreply.github.com>
2021-08-31 11:41:10 +0800

    远程改了东西
    远程改了东西

--- src/main/java/com/mxp/Hello.java
+++ src/main/java/com/mxp/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
         System.out.println("abc");
         System.out.println("dev11111111111");
         System.out.println("我在测试git的push操作");
+        System.out.println("我在远程改了东西");
 
     }
 }
