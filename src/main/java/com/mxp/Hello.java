a0a56e3a4a97b781ec6aa77c606ae17940ce13fd
mengxiaobin <mengxiaobin@calfcredit.com>
2021-09-01 11:48:55 +0800

    提交了不少

--- src/main/java/com/mxp/Hello.java
+++ src/main/java/com/mxp/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
         System.out.println("我在测试git的push操作");
         System.out.println("我在远程改了东西");
         System.out.println("111111");
+        System.out.println("22222222222");
 
     }
 }
